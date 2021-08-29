90e3a4a5fe54eaf67bf87a5c8436ad99d8ba637f
tehjawn <tehjawn@gmail.com>
2021-08-29 16:45:05 -0400

    Basic Home & Login Page Router Implementation

--- allevi/src/App.js
+++ allevi/src/App.js
@@ -5,9 +5,52 @@
  * 2. See User's Printers
  */
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom'
+
+let Login = () => (
+  <div>
+    Login
+  </div>
+)
+
+let Home = () => (
+  <div>
+    Home
+  </div>
+)
+
 function App() {
   return (
     <div className="App">
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
